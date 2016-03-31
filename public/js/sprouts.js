0ff6af47284e63807f57377f5292a80e212dc7a6
Ashley <ashhamp27@gmail.com>
2016-03-31 14:59:04 -0400

    adds infinite scroll

--- public/js/sprouts.js
+++ public/js/sprouts.js
@@ -1,22 +1,22 @@
 var pageNumber = 1;
 
-$('.more-sprouts').on('click', function(event) {
-  event.preventDefault();
+  $(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() == $(document).height()) {
+     pageNumber++;
 
-    pageNumber++;
+     var request = $.ajax({
+       method: "GET",
+       url: "/tweets.json",
+       data: { page: pageNumber }
+     }).done(function(tweets) {
 
-    var request = $.ajax({
-      method: "GET",
-      url: "/tweets.json",
-      data: { page: pageNumber }
-    }).done(function(tweets) {
-
-      tweets.forEach(function(tweet) {
-        var lineBegin = "<li class='tweet'>";
-        var lineEnd = "</li>";
-        var body = "<div class='body'>" + tweet["text"] + "</div>";
-        var username = "<div class='user'>" + tweet["username"] + "</div>";
-        $(".tweets").append(lineBegin + body + username + lineEnd);
-      });
-    });
-  });
+       tweets.forEach(function(tweet) {
+         var lineBegin = "<li class='tweet'>";
+         var lineEnd = "</li>";
+         var body = "<div class='body'>" + tweet["text"] + "</div>";
+         var username = "<div class='user'>" + tweet["username"] + "</div>";
+         $(".tweets").append(lineBegin + body + username + lineEnd);
+       });
+     });
+   }
+});
